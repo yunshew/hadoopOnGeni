aabeaa554c555170533e8ab30ab690f1b9055a79
yunshew <34481530+yunshew@users.noreply.github.com>
2018-03-19 11:05:49 -0400

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -5,6 +5,13 @@ sudo python get-pip.py
 sudo pip install --upgrade protobuf 
 sudo pip install numpy h5py paramiko pbcore py4j pyspark
 
+#All hosts
+wget http://apache.claz.org/spark/spark-2.3.0/spark-2.3.0-bin-hadoop2.7.tgz -P /tmp
+sudo tar zxvf /tmp/spark-2.3.0-bin-hadoop2.7.tgz -C /opt
+sudo cp /opt/spark-2.3.0-bin-hadoop2.7/conf/spark-env.sh.template /opt/spark-2.3.0-bin-hadoop2.7/conf/spark-env.sh
+sudo echo "JAVA_HOME=/usr/java/default" >> /opt/spark-2.3.0-bin-hadoop2.7/conf/spark-env.sh
+sudo chown -R yunshew /opt/spark-2.3.0-bin-hadoop2.7
+
 # All Worker Node
 if ! [ "$(echo $(hostname) | cut -d. -f1)" = "namenode" ]; then
   # Download SMRT-analysis
